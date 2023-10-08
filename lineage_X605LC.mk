e3eda36c679572decc49c1cd1a51b966715055a0
Nikhil2006 <nikhilradder9@gmail.com>
2023-10-08 19:37:24 +0530

    Added lineage make file and Vendorsetup

---
+++ lineage_X605LC.mk
@@ -0,0 +1,48 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1200
+TARGET_SCREEN_WIDTH := 1920
+
+# TODO: add this file
+# Inherit 64-bit configs from AOSP
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# TODO: Change this
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lenovo/X605LC/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_X605LC
+PRODUCT_DEVICE := X605LC
+PRODUCT_BRAND := Lenovo
+PRODUCT_MODEL := Lenovo TB-X605LC
+PRODUCT_MANUFACTURER := LENOVO
+
+TARGET_OTA_ASSERT_DEVICE := X605,X605LC,TB-X605LC
+
+PRODUCT_GMS_CLIENTID_BASE := android-lenovo
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	PRIVATE_BUILD_DESC="hq_sdm450-user 9 PKQ1.181203.001 646 release-keys" \
+	TARGET_DEVICE="X605LC"
+
+BUILD_FINGERPRINT := Lenovo/LenovoTB-X605LCV/TB-X605LC:9/PKQ1.181203.001/092_201021_IN:user/release-keys
+
---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+add_lunch_combo lineage_X605LC-eng
+add_lunch_combo lineage_X605LC-userdebug
+add_lunch_combo lineage_X605LC-user
